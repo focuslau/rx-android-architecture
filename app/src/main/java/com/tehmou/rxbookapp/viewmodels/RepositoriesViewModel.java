ad149f9c1a07c80be504184f3414481f5932840b
Timo Tuominen <timo.tuominen@gmail.com>
2015-01-12 00:29:17 +0000

    Add RepositoriesViewModel and adjust the stores

--- app/src/main/java/com/tehmou/rxbookapp/viewmodels/RepositoriesViewModel.java
+++ app/src/main/java/com/tehmou/rxbookapp/viewmodels/RepositoriesViewModel.java
@@ -1,11 +1,18 @@
 package com.tehmou.rxbookapp.viewmodels;
 
+import android.util.Log;
+
 import com.tehmou.rxbookapp.data.DataLayer;
 import com.tehmou.rxbookapp.pojo.GitHubRepository;
+import com.tehmou.rxbookapp.pojo.GitHubRepositorySearch;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import rx.Observable;
+import rx.functions.Func1;
+import rx.functions.FuncN;
 import rx.subjects.BehaviorSubject;
 import rx.subjects.Subject;
 import rx.subscriptions.CompositeSubscription;
@@ -15,6 +22,7 @@ import rx.subscriptions.CompositeSubscription;
  * Created by ttuo on 19/03/14.
  */
 public class RepositoriesViewModel {
+    private static final String TAG = RepositoriesViewModel.class.getSimpleName();
     final private CompositeSubscription compositeSubscription = new CompositeSubscription();
 
     final private DataLayer dataLayer;
@@ -33,7 +41,32 @@ public class RepositoriesViewModel {
     }
 
     public void subscribeToDataStore() {
-        compositeSubscription.add(dataLayer.getGitHub(search).subscribe(repositories));
+        compositeSubscription.add(
+                dataLayer.getGitHubRepositorySearch(search)
+                        .flatMap((repositorySearch) -> {
+                            final List<Observable<GitHubRepository>> observables = new ArrayList<>();
+                            for (String repositoryId : repositorySearch.getItems()) {
+                                Log.d(TAG, "Process repositoryId: " + repositoryId);
+                                final Observable<GitHubRepository> observable =
+                                        dataLayer.getGitHubRepository(repositoryId);
+                                observable.subscribe((repository) -> {
+                                           Log.d(TAG, repository.toString());
+                                        });
+                                observables.add(observable);
+                            }
+                            return Observable.combineLatest(
+                                    observables,
+                                    (args) -> {
+                                        Log.v(TAG, "Combine items into a list");
+                                        final List<GitHubRepository> list = new ArrayList<>();
+                                        for (Object repository : args) {
+                                            list.add((GitHubRepository) repository);
+                                        }
+                                        return list;
+                                    }
+                            );
+                        })
+                       .subscribe(repositories));
     }
 
     public void unsubscribeFromDataStore() {
